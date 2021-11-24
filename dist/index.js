a874d119212543168db2734395848b5bced2135d
Ali Zali <ali.zali321@gmail.com>
2021-11-25 01:27:35 +0330

    feat(config): add script to package.json

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function main() {
+    const url = "https://www.accuweather.com/en/gb/london/ec4a-2/november-weather/328328?year=2021";
+    console.log(url);
+}
+main();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+function main() {
+  const url =
+    "https://www.accuweather.com/en/gb/london/ec4a-2/november-weather/328328?year=2021";
+}
+
+main();
