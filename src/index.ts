d429310ea2541c98d083b6c2b6a16d6561da6f34
Ali Zali <ali.zali321@gmail.com>
2021-11-25 04:00:46 +0330

    feat: seperate day and night

--- src/index.ts
+++ src/index.ts
@@ -38,35 +38,46 @@ async function main() {
       if (!err) {
         let dailyInformation = [];
         for (var i = 0; i < results.length; i++) {
+          console.log(i);
+
           const $ = cheerio.load(results[i]);
           const day = $(".half-day-card.content-module");
-          let description: any = [];
-          let feature: any = {};
+          let weatherDescription: any = [];
+          let weatherFeature: any = [];
 
           day.each(function () {
             $(this)
               .find(".phrase")
               .each(function (index, element) {
-                description.push($(element).text());
+                weatherDescription.push($(element).text());
               });
             $(this)
-              .find(".panel-item")
+              .find(".panels")
               .each(function (index, element) {
-                const featuresText = $(element).text();
-                const featureValue = $(element).children().text();
-                feature[
-                  camelCase(
-                    featuresText.substr(0, featuresText.indexOf(featureValue))
-                  )
-                ] = featureValue;
+                let feature: any = {};
+                $(element)
+                  .find(".panel-item")
+                  .each(function (index, element) {
+                    const featuresText = $(element).text();
+                    const featureValue = $(element).children().text();
+                    feature[
+                      camelCase(
+                        featuresText.substr(
+                          0,
+                          featuresText.indexOf(featureValue)
+                        )
+                      )
+                    ] = featureValue;
+                  });
+                weatherFeature.push(feature);
               });
           });
           const today = new Date();
           const newDay = today.setDate(today.getDate() + i);
           dailyInformation.push({
             date: new Date(newDay).toDateString(),
-            description: description[0],
-            ...feature,
+            day: { description: weatherDescription[0], ...weatherFeature[0] },
+            night: { description: weatherDescription[1], ...weatherFeature[1] },
           });
         }
         writeFile(
