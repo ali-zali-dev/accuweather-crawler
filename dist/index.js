43f00b668d45020876ccd9becf60436c93cbc78d
Ali Zali <ali.zali321@gmail.com>
2021-11-28 09:35:05 +0330

    feat(git): add result and dist to gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function main() {
-    const url = "https://www.accuweather.com/en/gb/london/ec4a-2/november-weather/328328?year=2021";
-    console.log(url);
-}
-main();
